46ebf46b53da8591ed7b8daeeb43e7fab1dcc7bf
Mohammed23200 <mgsa112233@gmail.com>
2025-07-03 05:20:55 +0300

    game over 2

--- main.py
+++ main.py
@@ -25,11 +25,17 @@ while game_is_on:
     snake.move()
     if snake.head.distance(food)<15:
         food.refresh()
+        snake.extend()
         score.increase_score()
     if snake.head.xcor()>280 or snake.head.xcor()<-280 or  snake.head.ycor()>280 or snake.head.ycor()<-280:
           
         game_is_on=False
         score.game_over()
-
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) <10:
+            game_is_on=False
+            score.game_over()
      
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -12,36 +12,42 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
-        self.head=self.segments[0]
-
+        self.head = self.segments[0]
 
     def create_snake(self):
         for i in range(3):
-            new_square = Turtle(shape="square")
-            
-            new_square.penup()
-            new_square.color("white")
-            new_square.goto(x=X_POSITION[i], y=0)
-            self.segments.append(new_square)
+            self.add_segment((X_POSITION[i], 0))  # Pass position tuple
 
     def move(self):
         for square_num in range(len(self.segments) - 1, 0, -1):
             newX = self.segments[square_num - 1].xcor()
             newY = self.segments[square_num - 1].ycor()
             self.segments[square_num].goto(x=newX, y=newY)
-        self.segments[0].forward(MOVE_DISTANCE)  
+        self.segments[0].forward(MOVE_DISTANCE)
+
     def up(self):
         if self.segments[0].heading() != DOWN:
-
             self.segments[0].setheading(UP)
+
     def down(self):
         if self.segments[0].heading() != UP:
             self.segments[0].setheading(DOWN)
+
     def left(self):
         if self.segments[0].heading() != RIGHT:
             self.segments[0].setheading(LEFT)
+
     def right(self):
         if self.segments[0].heading() != LEFT:
             self.segments[0].setheading(RIGHT)
-    
-    
+
+    def add_segment(self, position):
+        new_square = Turtle(shape="square")
+        new_square.penup()
+        new_square.color("white")
+        new_square.goto(position)
+        self.segments.append(new_square)
+
+    def extend(self):
+        # Add new segment at the last segment's position
+        self.add_segment(self.segments[-1].position())
