2d83dd8c3853d0c9adb221ec3efa023650c20777
Mohammed23200 <mgsa112233@gmail.com>
2025-07-03 05:10:43 +0300

    game over 1

--- main.py
+++ main.py
@@ -26,6 +26,10 @@ while game_is_on:
     if snake.head.distance(food)<15:
         food.refresh()
         score.increase_score()
+    if snake.head.xcor()>280 or snake.head.xcor()<-280 or  snake.head.ycor()>280 or snake.head.ycor()<-280:
+          
+        game_is_on=False
+        score.game_over()
 
      
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -6,13 +6,19 @@ class Score(Turtle):
         self.score = 0
         self.hideturtle()
         self.penup()
-        self.goto(x=0,y=270)
+        self.goto(x=0, y=270)
         self.color("white")
         self.update_score_board()
+
     def update_score_board(self):
-        self.write(f"score: {self.score}",align="center",font=('Arial',24,'normal'))
+        self.clear()
+        self.write(f"Score: {self.score}", align="center", font=('Arial', 24, 'normal'))
 
     def increase_score(self):
-        self.score+=1
-        self.clear()
+        self.score += 1
         self.update_score_board()
+    
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over", align="center", font=("Arial", 24, "normal"))
+
